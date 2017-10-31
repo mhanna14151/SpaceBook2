92c69784cbcf27c4618eabd11b2cbc815a7196d9
Miles Benjamin <louren.of.courone@gmail.com>
2017-10-31 12:04:34 -0400

    Got the nasa image search working

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,13 @@
 import {Routes, RouterModule} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
-import {NasaTestComponent} from "./nasa-test/nasa-test.component";
+import {NasaTestComponent} from './nasa-test/nasa-test.component';
+import {NasaPictureTestComponent} from './nasa-picture-test/nasa-picture-test.component';
 // Import all other components here
 
 const APP_ROUTES: Routes = [
   {path: 'nasa', component: NasaTestComponent},
   {path: '', component: NasaTestComponent},
+  {path: 'nasa-pic', component: NasaPictureTestComponent}
   // so on
 ];
 
--- src/app/nasa-picture-test/nasa-picture-test.component.ts
+++ src/app/nasa-picture-test/nasa-picture-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NasaServiceClient} from "../services/nasa.service.client";
 
 @Component({
   selector: 'app-nasa-picture-test',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nasa-picture-test.component.css']
 })
 export class NasaPictureTestComponent implements OnInit {
-
-  constructor() { }
+  searchParams: String;
+  result: Object;
+  gotResult: Boolean;
+  search(params: String) {
+    this.nasaService.searchImg(this.searchParams)
+      .subscribe((result) => {
+        this.result = result;
+        this.gotResult = true;
+        console.log(result);
+      });
+  }
+  constructor(private nasaService: NasaServiceClient) { }
 
   ngOnInit() {
+    this.gotResult = false;
   }
 
 }
