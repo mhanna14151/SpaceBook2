61ba6a19f4fdbc1a3231e34b0d5a117d00e6d79c
jennyjohns <johns.j@husky.neu.edu>
2017-10-31 13:42:01 -0400

    debuggin app modules

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,17 @@
 import {Routes, RouterModule} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
-<<<<<<< HEAD
 import {NasaTestComponent} from './nasa-test/nasa-test.component';
 import {NasaPictureTestComponent} from './nasa-picture-test/nasa-picture-test.component';
-// Import all other components here
-
-const APP_ROUTES: Routes = [
-  {path: 'nasa', component: NasaTestComponent},
-  {path: '', component: NasaTestComponent},
-  {path: 'nasa-pic', component: NasaPictureTestComponent}
-=======
-import {NasaTestComponent} from "./nasa-test/nasa-test.component";
 import {FrontPageComponent} from "./front-page/front-page.component";
-// Import all other components here
+
 
 const APP_ROUTES: Routes = [
   {path: 'searchAsteroids', component: NasaTestComponent},
   {path: '', component: FrontPageComponent},
->>>>>>> jj
-  // so on
-];
+  {path: 'nasa-pic', component: NasaPictureTestComponent},
+  ]
+
+
 
 // Export the routes as module providers
 export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
