7b833d951c14090712881fe758a45b26969fa437
jennyjohns <johns.j@husky.neu.edu>
2017-10-30 22:44:24 -0400

    node modules

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('*', function (req, res) {
 });
 
 
-server.listen( port , () => console.log('Running'));
+// server.listen( port , () => console.log('Running'));
 
 // require("./server/app");
 // app.listen(port, ipaddress);
