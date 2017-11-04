a30a013bc09e8ab47d28396a37cd45684f50a2e2
jennyjohns <johns.j@husky.neu.edu>
2017-11-04 16:25:09 -0400

    Working on adding users to the SpaceBook database on mongoose

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('f') profileForm: NgForm;
+  userId: string;
+  user = {};
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute, private router: Router) {
   }
 
+  ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['uid'];
+        }
+      );
+    this.userService.findUserById(this.userId)
+      .subscribe((user: any) => {
+        this.user = user;
+        this.username = this.user['username'];
+        this.firstName = this.user['firstName'];
+        this.password = this.user['password'];
+        this.lastName = this.user['lastName'];
+        this.email = this.user['email'];
+      });
+  }
+
+  websites() {
+    this.router.navigate(['user/', this.userId, 'website']);
+  }
+
+  goToProfile(uname, email, fname, lname) {
+    const user = {username: uname, email: email, firstName: fname, lastName: lname};
+    this.userService.updateUser(this.userId, user)
+      .subscribe((user1) => {
+      });
+  }
+
+  cancelChanges() {
+    this.ngOnInit();
+  }
+
+  deleteAccount(userId: string) {
+    this.userService.deleteUser(userId)
+      .subscribe((users) => {
+        this.router.navigate(['/login']);
+      });
+  }
 }
--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {UserService} from '../../../services/user.service.client';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') registrationForm: NgForm;
+  username: string;
+  password: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  errorFlag: boolean;
+  errorMsg: string;
+  user: any;
+  user_id: string;
+  constructor(private userService: UserService, private router: Router) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+  }
+  register(username: string, password: string, email: string, firstName: string, lastName: string) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.errorMsg = 'Invalid username or password!';
+    this.errorFlag = false;
+  }
+  registered(username, password, firstName, lastName, email) {
+    this.userService.findUserByUsername(username)
+      .subscribe((user: any) => {
+        if (user !== null) {
+          this.errorFlag = true;
+          this.errorMsg = 'Username already in use, please choose another username!';
+        }else {
+          const user1 = {username: username, password: password, firstName: firstName, lastName: lastName, email: email};
+          this.userService.createUser(user1)
+            .subscribe((user2) => {
+              this.user = user2;
+              this.user_id = user2['_id'];
+              this.router.navigate(['user/', this.user_id]);
+            });
+        }
+      });
   }
 
 }
