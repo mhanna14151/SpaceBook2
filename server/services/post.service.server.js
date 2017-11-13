f6fa54b3375fd14699c2afcc2f5dfa955214f65f
Miles Benjamin <louren.of.courone@gmail.com>
2017-11-13 15:31:07 -0500

    post server and post model stuff

--- server/services/post.service.server.js
+++ server/services/post.service.server.js
@@ -1,14 +1,14 @@
 module.exports = function (app) {
-  var postModel = require("../../model/post/post.model.server");
+  var postModel = require('../../model/post/post.model.server');
 
-  app.get("/api/post/:pid", findPostById);
-  app.get("/api/post", findPosts);
-  app.post("/api/post", createPost);
-  app.put("/api/post/:pid", updatePost);
-  app.delete("/api/post/:pid", deletePost);
+  app.get('/api/post/:pid', findPostById);
+  app.get('/api/post', findPosts);
+  app.post('/api/post', createPost);
+  app.put('/api/post/:pid', updatePost);
+  app.delete('/api/post/:pid', deletePost);
 
   function findPostById(req, res) {
-    var postId = req.params["pid"];
+    var postId = req.params['pid'];
     postModel.findPostById(postId)
       .then(function (post) {
         res.json(post);
@@ -16,24 +16,10 @@ module.exports = function (app) {
   }
 
   function findPosts(req, res) {
-    var postname = req.query["postname"];
-    var password = req.query["password"];
-    if (postname && password) {
-      postModel.findPostByCredentials(postname, password)
-        .then(function (post) {
-          res.json(post);
-        });
-      return;
-    }
-    else if (postname) {
-      postModel
-        .findPostByPostname(postname)
-        .then(function (post) {
-          res.json(post);
-        });
-      return;
-    }
-    res.json(posts);
+    var tag = req.query['tag'];
+      postModel.findPostsByTag(tag).then(function (posts) {
+        res.json(posts);
+      });
   }
 
   function createPost(req, res) {
@@ -56,12 +42,10 @@ module.exports = function (app) {
   }
 
   function deletePost(req, res) {
-    var postId = req.params['uid'];
-    var post = posts.find(function (post) {
-      return postId === post._id
+    var postId = req.params['pid'];
+    postModel.deletePost(postId).then(function (any) {
+      res.json(null);
     });
-    var i = posts.indexOf(post);
-    posts.splice(i, 1);
-    res.json(posts);
+
   }
 };
