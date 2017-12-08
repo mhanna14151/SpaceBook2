5cb09a796139edf7a87aeacfa59ff1137a7e3808
Michael Hanna <31774802+mhanna14151@users.noreply.github.com>
2017-12-08 16:03:09 -0500

    Update model.server.js

--- model/model.server.js
+++ model/model.server.js
@@ -1,12 +1,12 @@
+var mongoose = require("mongoose");
 var connectionString = 'mongodb://localhost:/cs5610'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds133876.mlab.com:33876/heroku_1g3mhf13'; // use yours
+  connectionString += '@ds133746.mlab.com:33746/heroku_p2mbwstg'; // use yours
 }
 
-var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString);
 
 module.exports = db;
